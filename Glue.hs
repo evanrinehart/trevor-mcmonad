e5dfcfb612a61613529d407222d8d749dcbdb3ff
EvanR <evanrinehart@gmail.com>
2015-03-16 20:25:51 -0500

    joystick support

--- Glue.hs
+++ Glue.hs
@@ -9,9 +9,10 @@ import System.Exit
 import Data.IORef
 import Data.Vector (Vector)
 import qualified Data.Vector as V
-import Control.Monad (when, forever)
+import Control.Monad (when, forever, mapM, forM, forM_, replicateM)
 import Control.Concurrent
 import Control.Applicative
+import Data.Maybe
 
 import Graphics.UI.GLFW 
 import Control.Broccoli
@@ -22,19 +23,19 @@ data GlfwInputs = GlfwInputs
   , glfwCursorPos :: X (Double,Double)
   , glfwMouseButton :: E (MouseButton, MouseButtonState, ModifierKeys)
   , glfwScroll :: E (Double,Double)
-  , glfwJoysticks :: Vector GlfwJoystickInput -- ^ there are 16 joysticks
+  , glfwJoysticks :: [GlfwJoystickInput] -- ^ there are 16 joysticks
   , glfwFramebufferResize :: E (Int,Int)
   , glfwClose :: E ()
   }
 
 data GlfwJoystickInput = GlfwJoystickInput
-  { glfwJoystickAppearEvent :: E String -- ^ name of joystick
-  , glfwJoystickLeaveEvent :: E ()
+  { glfwJoystickPresent :: X (Maybe String) -- ^ name of joystick
+  , glfwJoystickButtons :: X [JoystickButtonState]
   , glfwJoystickAxes :: X [Double]
-  , glfwJoystickButtonEvent :: E (Int, JoystickButtonState) }
+  }
 
 nullJoystick :: GlfwJoystickInput
-nullJoystick = GlfwJoystickInput never never (pure [0,0]) never
+nullJoystick = GlfwJoystickInput (pure Nothing) (pure []) (pure [])
 
 errorCb :: Error -> String -> IO ()
 errorCb err msg = do
@@ -77,11 +78,18 @@ runGlfw winW winH title initProc renderProc setup = do
       vsyncMV <- newEmptyMVar
       closeMV <- newEmptyMVar
       graphicsMV <- newEmptyMVar
+      joyPresentMV <- replicateM 16 newEmptyMVar
+      joyButtonMV <- replicateM 16 newEmptyMVar
+      joyAxisMV <- replicateM 16 newEmptyMVar
       setWindowSizeCallback  win (Just (sizeCb (putMVar windowMV)))
       setKeyCallback         win (Just (keyCb (putMVar keyMV)))
       setCharCallback        win (Just (charCb (putMVar charMV)))
       setMouseButtonCallback win (Just (clickCb (putMVar clickMV)))
       setCursorPosCallback   win (Just (mouseCb (putMVar mouseMV)))
+      let x1 = map (\i mname -> putMVar (joyPresentMV !! i) mname) [0..15]
+      let x2 = map (\i numStats -> putMVar (joyButtonMV !! i) numStats) [0..15]
+      let x3 = map (\i axes -> putMVar (joyAxisMV !! i) axes) [0..15]
+      killJoysticks <- joystickMonitor x1 x2 x3
       initProc
       _ <- forkIO $ do
         runProgram $ \onBoot time -> do
@@ -99,7 +107,14 @@ runGlfw winW winH title initProc renderProc setup = do
           input (forever (takeMVar windowMV >>= resize))
           input (forever (takeMVar vsyncMV >>= vsync))
           input (forever (takeMVar closeMV >>= close))
-          let joysticks = V.replicate 16 nullJoystick
+          joysticks <- forM [0..15] $ \i -> do
+            (present, setPresent) <- newX Nothing
+            (buttons, setButtons) <- newX []
+            (axes, setAxes) <- newX []
+            input (forever (takeMVar (joyPresentMV !! i) >>= setPresent))
+            input (forever (takeMVar (joyButtonMV !! i) >>= setButtons))
+            input (forever (takeMVar (joyAxisMV !! i) >>= setAxes))
+            return (GlfwJoystickInput present buttons axes)
           let glfwIns = GlfwInputs onKey onChar mouse onClick never
                   joysticks onResize onClose
           (scene, exit) <- setup glfwIns onBoot time
@@ -107,6 +122,7 @@ runGlfw winW winH title initProc renderProc setup = do
             (\_ x -> putMVar graphicsMV (Just (renderProc x)))
             (snapshot_ onVsync scene)
           return exit
+        killJoysticks
         putMVar graphicsMV Nothing
       forever $ do
         pollEvents
@@ -150,3 +166,57 @@ clickCb go _ button state mods = go (button,state,mods)
 mouseCb :: ((Double,Double) -> IO ()) -> Window -> Double -> Double -> IO ()
 mouseCb go _ x y = go (x,y)
 
+
+
+joystickMonitor :: [Maybe String -> IO ()]
+                -> [[JoystickButtonState] -> IO ()]
+                -> [[Double] -> IO ()]
+                -> IO (IO ())
+joystickMonitor cb0 cb1 cb2 = do
+  pollers <- replicateM 16 newEmptyMVar
+  myTid <- forkIO . forever $ do
+    forM_ [0..15] $ \i -> do
+      b <- joystickPresent (toEnum i)
+      when b $ do
+        tid <- forkIO (joystickPoller (toEnum i) (cb1 !! i) (cb2 !! i))
+        mprev <- tryTakeMVar (pollers !! i)
+        case mprev of
+          Nothing -> putMVar (pollers !! i) tid
+          Just prev -> do
+            killThread prev
+            putMVar (pollers !! i) tid
+        mname <- getJoystickName (toEnum i)
+        (cb0 !! i) (Just (fromMaybe "" mname))
+      when (not b) $ do
+        mprev <- tryTakeMVar (pollers !! i)
+        case mprev of
+          Nothing -> return ()
+          Just prev -> killThread prev
+        (cb0 !! i) Nothing
+    threadDelay 2000000
+  return $ do
+    killThread myTid
+    forM_ pollers $ \mv -> do
+      mtid <- tryTakeMVar mv
+      case mtid of
+        Just tid -> killThread tid
+        Nothing -> return ()
+
+-- poll joystick axes and buttons at "high enough speed"
+joystickPoller :: Joystick
+               -> ([JoystickButtonState] -> IO ())
+               -> ([Double] -> IO ())
+               -> IO ()
+joystickPoller jnum buttonChange axisChange = do
+  forever $ do
+    mbts <- getJoystickButtons jnum
+    case mbts of
+      Nothing -> return ()
+      --Just bts -> buttonChange bts
+      Just bts -> return ()
+    maxes <- getJoystickAxes jnum
+    case maxes of
+      Nothing -> return ()
+      --Just axes -> axisChange axes
+      Just axes -> return ()
+    threadDelay 20000
